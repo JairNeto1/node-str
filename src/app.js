38900013583a67b475eeb8bbb8f500dba9c48704
JairNeto1 <jairmonteironeto@gmail.com>
2021-05-19 10:50:01 -0300

    Adding the Mongoose database

--- src/app.js
+++ src/app.js
@@ -2,10 +2,17 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 const router = express.Router();
 
+//conectar ao banco
+mongoose.connect(
+  "mongodb+srv://admin:1234@cluster0.4nvhz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+);
+
+// carrega as rotas
 const indexRoute = require("./routes/index-route");
 const productRoute = require("./routes/product-route");
 
